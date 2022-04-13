faba0c2bec390b9d2c51dfbfa072970720973646
wulalala <2011470458@qq.com>
2022-04-13 11:02:47 +0800

    test2

--- data_struct/class1/class1.cpp
+++ data_struct/class1/class1.cpp
@@ -1,1 +1,11 @@
 #include <iostream>
+int main(int argc, char** argv)
+{
+    int i;
+    char array[10] = "asdfghjjk";
+    for(i = 0; i < 10; i++)
+    {
+        std::cout << "The address of array " << i << " is " << array[i] << "  " << (int *)&(array[i]) << std::endl;
+    }
+    return 0;
+}
