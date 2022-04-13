c1026ffda28bf64c938a2c96c5569ee352f40f91
wulalala <2011470458@qq.com>
2022-04-13 16:29:57 +0800

    a little trick

---
+++ data_struct/class1/trick.cpp
@@ -0,0 +1,15 @@
+inline int read()
+{
+    int X=0; bool flag=1; char ch=getchar();
+    while(ch<'0'||ch>'9') {if(ch=='-') flag=0; ch=getchar();}
+    while(ch>='0'&&ch<='9') {X=(X<<1)+(X<<3)+ch-'0'; ch=getchar();} 
+    if(flag) return X;
+    return ~(X-1);
+}
+
+inline void write(int X)
+{
+    if(X<0) {X=~(X-1); putchar('-');}
+    if(X>9) write(X/10);
+    putchar(X%10+'0');
+}
